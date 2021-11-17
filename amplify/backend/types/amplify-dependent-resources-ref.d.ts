932aa34bb01c38b1f6d71303da0f90c87801ccca
rhab <arteagarh@yahoo.com>
2021-11-16 21:57:22 -0500

    datastore

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,33 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "myapi": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "reactamplifiede3bb53c5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Triggeraf2bac3a": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "s3testrhab": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
